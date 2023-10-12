c21b0f1a2dd605573967e05d50cb3e46354f783c
2374628445 <2374628445@qq.com>
2023-10-12 15:58:32 +0800

    updata

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,6 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
+  title: '',
   description: '这是我的第一个 VuePress 站点',
 })
